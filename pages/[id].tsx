dd9946b78852387b3be1fecc45af3ca0cdc39bdb
inam95 <mfihassan99@gmail.com>
2022-08-07 19:29:34 +0530

    setup supabase client | fetch data from supabase | display

---
+++ pages/[id].tsx
@@ -0,0 +1,72 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+
+import Head from 'next/head';
+import { ParsedUrlQuery } from 'querystring';
+import supabaseClient from '../utils/supabaseClient';
+
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data, error } = await supabaseClient().from('lesson').select('id');
+
+  if (error) {
+    throw new Error('Something went wrong while fetching lesson Ids', {
+      cause: error as unknown as Error
+    });
+  }
+
+  const paths = data.map(({ id }) => ({
+    params: {
+      id: id.toString()
+    }
+  }));
+
+  return {
+    paths,
+    fallback: false
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, Params> = async (context) => {
+  const { id } = context.params!;
+  const { data: lesson, error } = await supabaseClient()
+    .from('lesson')
+    .select('*')
+    .eq('id', id)
+    .single();
+
+  if (error) {
+    throw new Error('Something went wrong while fetching the lesson', {
+      cause: error as unknown as Error
+    });
+  }
+
+  console.log(typeof lesson.created_at);
+
+  return {
+    props: {
+      lesson
+    }
+  };
+};
+
+const LocationDetails: NextPage = ({ lesson }) => {
+  return (
+    <div>
+      <Head>
+        <title>Let&apos;s Learn</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="w-full max-w-3xl px-8 py-16 mx-auto">
+          <h1 className="mb-6 text-3xl">{lesson.title}</h1>
+          <p>{lesson.description}</p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default LocationDetails;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,41 @@
+import type { GetStaticProps, NextPage } from 'next';
+
 import Head from 'next/head';
 import Image from 'next/image';
-import type { NextPage } from 'next';
+import Link from 'next/link';
+import supabaseClient from '../utils/supabaseClient';
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps<{ lessons: any[] }> = async () => {
+  const { data: lessons, error } = await supabaseClient()
+    .from('lesson')
+    .select('*');
+  if (error) {
+    throw new Error('Something went wrong while fetching lessons', {
+      cause: error as unknown as Error
+    });
+  }
+  return {
+    props: {
+      lessons
+    }
+  };
+};
+
+const Home: NextPage<{ lessons: any[] }> = ({ lessons }) => {
   return (
     <div>
       <Head>
         <title>Let&apos;s Learn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-3xl font-bold">Welcome to Let&apos;s Learn</h1>
+      <main className="w-full max-w-3xl px-2 mx-auto my-16">
+        {lessons.map((lesson) => (
+          <Link key={lesson.id} href={`/${lesson.id}`}>
+            <a className="flex h-40 p-8 mb-4 text-xl font-bold rounded shadow">
+              {lesson.title}
+            </a>
+          </Link>
+        ))}
       </main>
     </div>
   );
